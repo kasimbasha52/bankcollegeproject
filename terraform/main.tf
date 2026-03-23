b896ee348beb114f7d91328399323ddc72bf7034
kasimbasha52 <bashakasim527@gmail.com>
2026-03-23 19:05:29 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,11 @@
-# 1. Define the Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-# 2. Create a Random ID for unique bucket naming
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
 
-# 3. Create the S3 Bucket
-resource "aws_s3_bucket" "vault_test_bucket" {
-  bucket = "devsecops-vault-demo-${random_id.bucket_suffix.hex}"
-
-  tags = {
-    Name        = "Vault Dynamic Secret Test"
-    Environment = "Dev"
-    ManagedBy   = "Terraform"
-  }
+resource "aws_s3_bucket" "test_bucket" {
+  bucket = "vault-demo-bucket-${random_id.bucket_suffix.hex}"
 }
